cda2ed004f81dfb498690103f08d3461247c1d96
Sarvesh Kumar Sharma <55739302+shsarv@users.noreply.github.com>
2020-08-31 08:40:39 +0530

    Create Playing_with_character.c

---
+++ C/Playing_with_character.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+ char c,d[50],e[100];
+ scanf("%c",&c);
+ scanf("\n");
+ gets(d);
+ gets(e);
+ printf("%c\n",c);
+ printf("%s\n",d);
+ printf("%s",e);
+
+
+ return 0;
+}
+
