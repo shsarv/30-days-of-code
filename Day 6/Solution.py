8e8d8dc0b2db9ec3d8c8614f64d0159684d69767
Sarvesh Kumar Sharma <shsarv@gmail.com>
2020-09-21 23:46:41 +0530

    deleted

--- Day 6/Solution.py
+++
@@ -1,3 +0,0 @@
-for i in range(int(input())):
-    s=input()
-    print(s[0::2],s[1::2])
--- Day 7/Solution.py
+++
@@ -1,5 +0,0 @@
-n=int(input())
-l=list(map(int,input().split(" ")))
-l.reverse()
-for i in range(n):
-    print(l[i],end=" ")
