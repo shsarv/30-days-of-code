cd2e484c4b54d69b449f7fe4a80d3828c16b0184
Sarvesh Kumar Sharma <55739302+shsarv@users.noreply.github.com>
2020-09-22 23:28:05 +0530

    Create Solution.c

---
+++ Solution.c
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.util.*;
+
+class Node {
+	int data;
+	Node next;
+	Node(int d) {
+        data = d;
+        next = null;
+    }
+}
+
+class Solution {
+
+public static  Node insert(Node head,int data) {  if(head == null)
+        return new Node( data);
+    else if(head.next == null){
+            head.next = new Node(data);
+        }
+    else{
+        insert(head.next,data);
+    }
+
+
+        return head;
+
+        //Complete this method
+    }
+
+	public static void display(Node head) {
+        Node start = head;
+        while(start != null) {
+            System.out.print(start.data + " ");
+            start = start.next;
+        }
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        Node head = null;
+        int N = sc.nextInt();
+
+        while(N-- > 0) {
+            int ele = sc.nextInt();
+            head = insert(head,ele);
+        }
+        display(head);
+        sc.close();
+    }
+}
